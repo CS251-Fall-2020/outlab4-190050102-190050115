cbc2ce318589ecd4cf8bd57cd1a4a34a2ff03477
Sibasis Nayak <sibasisnayak07@gmail.com>
2020-09-09 23:10:17 +0530

    task4 and task5 done

---
+++ task4/task4.py
@@ -0,0 +1,68 @@
+import numpy as np
+from scipy.misc import derivative
+import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D
+
+def h(x):
+	if x>0:
+		return np.exp(-1/(x*x))
+	else:
+		return 0
+
+def g(x):
+	return (h(2-x)/(h(2-x) + h(x-1)))
+
+def b(x):
+	if x>0:
+		return g(x)
+	else:
+		return g(-x)
+
+@np.vectorize
+def sinc(x,y):
+	if x == 0 and y == 0:
+		return 0
+	else:
+		return np.sin(np.sqrt(x*x + y*y))/np.sqrt(x*x + y*y)
+
+def fn_plot1d(fn, x_min ,x_max ,filename):
+	x = np.linspace(x_min,x_max,num=1000)
+	plt.plot(x,map(fn,x))
+	plt.xlabel("x")
+	plt.ylabel("b(x)")
+	plt.title("b(x) vs x plot")
+	plt.savefig(filename)
+	plt.close()
+
+def fn_plot2d(fn, x_min, x_max, y_min, y_max, filename):
+	xx = np.linspace(x_min,x_max,num=100)
+	yy = np.linspace(y_min,y_max,num=100)
+	x,y = np.meshgrid(xx,yy)
+	z = fn(x,y)
+	ax = Axes3D(plt.gcf())
+	ax.plot_surface(x,y,z)
+	ax.set_xlabel("x")
+	ax.set_ylabel("y")
+	ax.set_zlabel("z")
+	plt.title("z = sincc(x,y) plot")
+	plt.savefig(filename)
+	plt.close()
+
+def nth_derivative_plotter(fn,n,xmin,xmax,filename):
+	xx = np.linspace(xmin,xmax)
+	y = [derivative(fn,x,n=n,dx=0.0001) for x in xx]
+	plt.plot(xx,y)
+	plt.xlabel("x")
+	plt.ylabel("$f^%i$(x)" %n)
+	plt.title("%ith derivative plot of f(x)" %n)
+	plt.savefig(filename)
+	plt.close()
+
+
+fn_plot1d(b,-2,2,"fn1plot.png")
+fn_plot2d(sinc,-1.5*np.pi,1.5*np.pi,-1.5*np.pi,1.5*np.pi,"fn2plot.png")
+nth_derivative_plotter(b,1,-2,2,"bd_1.png")
+nth_derivative_plotter(b,2,-2,2,"bd_2.png")
+
+
+
---
+++ task5/task5.py
@@ -0,0 +1,40 @@
+import csv
+import numpy as np
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy
+from scipy import stats
+
+csv_url = "https://api.covid19india.org/csv/latest/case_time_series.csv"
+
+df = pd.read_csv(csv_url)
+k = df[df['Date'] == '14 April '].index.values
+df.drop(df.index[:k[0]],inplace = True)
+
+deaths = df['Total Deceased'].tolist()
+n = len(deaths)
+
+y = []
+
+for i in range(1,n):
+	y.append(float(deaths[i])/float(deaths[i-1]))
+
+
+x = list(range(1,n))
+
+slope, intercept, r_value, p_value, std_err= stats.linregress(x,y)
+yline = [slope*xx + intercept for xx in x]
+
+plt.scatter(x, y, s=40, facecolors='none', edgecolors='b')
+plt.plot(x,yline,'r')
+plt.xlabel("t(days after 15 April)")
+plt.ylabel("H(t) = x(t)/x(t-1)")
+plt.title("H(t) vs t (Levitt's metric)")
+plt.savefig("covid.png")
+plt.close()
+
+day = int(np.ceil((1-intercept)/slope))
+print(day)
+
+
